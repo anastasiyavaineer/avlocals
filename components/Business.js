71c3a7c2301a052edeb9f824954d6d3ca9fa295e
anastasiyavaineer <vaineranastasiya091@gmail.com>
2022-02-08 03:39:00 +0800

    no message

--- components/Business.js
+++ components/Business.js
@@ -15,13 +15,13 @@ export default function Business({ business }) {
       <Link href={`/${business.slug}`}>
         <a className="flex">
           <div className="relative h-20 w-20 md:h-32 md:w-32">
-            {/* <Image
+            <Image
               src={url}
               alt={business.name}
               layout="fill"
               placeholder="blur"
               className="rounded object-cover "
-            /> */}
+            />
           </div>
           <div className="flex flex-col justify-center pl-4">
             <h1 className="text-lg md:text-xl font-bold text-gray-800 dark:text-white">
